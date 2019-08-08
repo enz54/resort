0cb4aa4fc3aeeafbd590e19842cf10500bd04b60
eric nzenga <enze3968@gmail.com>
2019-08-08 09:51:15 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div> Hello from home page</div>
+    )
+}
+
+export default Home;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const SingleRoom = () => {
+    return (
+        <div>Hello from single room</div>
+    )
+}
+
+export default SingleRoom;
